a8f6d82842864f624e8e9871aad8f1973953c383
backskin <backskin@outlook.com>
2020-03-01 23:57:03 +0400

    добавляем док-файл

--- TestScript.py
+++
@@ -1,6 +0,0 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QPushButton
-app = QApplication([])
-label = QLabel('Hello World!')
-button = QPushButton()
-label.show()
-app.exec_()
---
+++ test_script.py
@@ -0,0 +1,6 @@
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton
+APP = QApplication([])
+LABEL = QLabel('Hello World!')
+BUTTON = QPushButton()
+LABEL.show()
+APP.exec_()
