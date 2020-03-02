8d6bd5e2cf46d99a5e8b275e6dd06c2255c8d042
backskin <backskin@outlook.com>
2020-03-02 13:21:37 +0400

    0.0.0.1 test

--- test_script.py
+++ test_script.py
@@ -1,6 +1,28 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QPushButton
+from PyQt5.QtWidgets import *  # QApplication, QLabel, QPushButton
+
+import numpy as nump
+from cv2 import *
+import matplotlib.pyplot as plt
+
+
+def get_image():
+    cam = VideoCapture(0)
+    s, img = cam.read()
+    return img
+
+# plt.imshow(get_image())
+# plt.show()
+
 APP = QApplication([])
+window = QWidget()
+imlayout = QGraphicsItem()
+vbox = QVBoxLayout()
 LABEL = QLabel('Hello World!')
 BUTTON = QPushButton()
-LABEL.show()
+vbox.addWidget(LABEL)
+vbox.addWidget(BUTTON)
+window.setLayout(vbox)
+window.show()
+# LABEL.show()
+
 APP.exec_()
