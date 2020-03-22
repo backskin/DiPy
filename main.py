45c4af5ffd3670136da5dfc2672b60aa06ba7625
Vlad Bashkin <vbackskin@gmail.com>
2020-03-22 13:58:10 +0400

    Unknown commit

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
-from PyQt5.QtWidgets import *  # QApplication, QLabel, QPushButton
 
-import numpy as np
-from cv2 import *
-import matplotlib.pyplot as plt
+from PyQt5.QtWidgets import *
 
 
-def get_image():
-    cam = VideoCapture(0)
-    s, img = cam.read()
-    return img
+class SmartWindow:
+
+    def __init__(self, title: str):
+        self.main_window = QWidget()
+        self.main_window.setWindowTitle(title)
+
+    def __init__(self, title: str, width: int, height: int):
+        self.main_window = QWidget()
+        self.main_window.setWindowTitle(title)
+        self.set_size(width, height)
+
+    def set_size(self, width: int, height: int):
+        self.main_window.resize(width, height)
+        self.main_window.setFixedWidth(width)
+        self.main_window.setFixedHeight(height)
+
+    def set_scene(self, main_layout: QLayout):
+        self.main_window.setLayout(main_layout)
+
+    def show(self):
+        self.main_window.show()
 
 
 def main():
-    APP = QApplication([])
-    window = QWidget()
-    vbox = QVBoxLayout()
-    LABEL = QLabel('Hello World!')
-    BUTTON = QPushButton()
-    vbox.addWidget(LABEL)
-    vbox.addWidget(BUTTON)
-    window.setLayout(vbox)
+    import sys
+    app = QApplication(sys.argv)
+    window = SmartWindow('Main Window', 640, 480)
     window.show()
-    APP.exec_()
+    sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
     main()
-
 
