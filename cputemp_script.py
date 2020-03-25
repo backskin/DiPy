f56f6ce8183f7d46ab3736e0c1a4d2d748a54e1f
backskin <backskin@outlook.com>
2020-03-25 15:25:59 +0400

    new log and modified cputemp script

--- cputemp_script.py
+++ cputemp_script.py
@@ -10,35 +10,13 @@ def writeLog():
     log.write("{0} | {1} 'C\n".format(strftime("%H:%M:%S"),str(temp)))
     log.close()
 
-
-def update_the_day(day, cont):
-    if day != strftime("%Y%m%d"):
-        cont['x'] = []
-        cont['y'] = []
-        day = strftime("%Y%m%d")
-    else:
-        cont['y'].append(temp)
-        cont['x'].append(time())
+def main():
+    cpu = CPUTemperature()
+    while True:
+        temp = cpu.temperature
+        writeLog()
+        sleep(10)
         
 
-def draw_upd(cont):
-    plt.clf()
-    x = cont['x']
-    y = cont['y']
-    plt.scatter(x,y)
-    plt.plot(x,y)
-    plt.pause(1)
-    plt.draw()
-
-
-plt.ion()
-plt_contnr = {'x':[],'y':[]}
-cpu = CPUTemperature()
-day = strftime("%Y%m%d")
-while True:
-    temp = cpu.temperature
-    # update_the_day(day, plt_contnr)
-    writeLog()
-    # draw_upd(plt_contnr)
-    sleep(5)
-    day = strftime("%Y%m%d")
+if __name__ == "__main__":
+    main()
