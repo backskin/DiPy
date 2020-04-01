0e66890ab2d92bbc90a1aad0318c3375a6310bb6
backskin-pi <backskin@outlook.com>
2020-04-02 01:08:03 +0400

    auto-commit (RP)::probably temp only

--- cputemp_script.py
+++ cputemp_script.py
@@ -1,19 +1,11 @@
 from gpiozero import CPUTemperature
 from time import sleep, strftime, time
 import matplotlib.pyplot as plt
-import pwd
-import grp
-import os
-
 
 def writeLog(cpu):
     logname = "CPUtemp_"+strftime("%Y%m%d")+".log"
     fld_path = "/home/pi/DiPy/resources/templogs/"
     log = open(fld_path+logname, "a")
-    uid = pwd.getpwnam("pi").pw_uid
-    gid = grp.getgrnam("pi").gr_gid
-    os.chown(fld_path+logname,uid,gid)
-    os.chmod(fld_path+logname, 0o666)
     temp = cpu.temperature
     print(temp)
     log.write("{0} | {1} 'C\n".format(strftime("%H:%M:%S"),str(temp)))
@@ -24,7 +16,7 @@ def main():
     while True:
         writeLog(cpu)
         sleep(10)
-        
+
 
 if __name__ == "__main__":
     main()
--- update_loop.sh
+++ update_loop.sh
@@ -1,22 +1,20 @@
 #!/bin/sh
 echo "Update-Task: Start"
-cd /home/pi/DiPy
+cd /home/pi/DiPy;
 
 while /bin/true; do
     echo "Update-Task: Pull..."
-    git pull
+    git pull;
     echo "Trying to gather all data and commit"
     git add --all
-    echo "configuring GIT"
-    git config --l globauser.email "backskin@outlook.com"
-    git config --global user.name "backskin"
-    git config credentials.helper store
+    git config --system globauser.email "backskin@outlook.com"
+    git config --system user.name "backskin"
+    git config credentials.helper store;
     echo "Commit line";
     git commit --author "backskin"  -a -m "auto-commit (RP)::probably temp only"
-    
-    echo "Push line"
-    git push
-    echo "Update-Task: Objectives done"
-    echo "Waiting for 2 minutes..."
-    sleep 120
+    echo "Push line";
+    git push;
+    echo "Update-Task: Objectives done";
+    echo "Waiting for 2 minutes...";
+    sleep 120;
 done &
