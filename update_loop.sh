3dd46b54afa2fe8cda17f9c1ad0f6aadacb528ab
BackSkin-Pi <backskin@outlook.com>
2020-04-01 22:37:12 +0400

    auto-commit (RP)::probably temp only

--- update_loop.sh
+++ update_loop.sh
@@ -2,13 +2,13 @@
 cd /home/pi/DiPy
 echo "Update-Task: Start"
 git config --global user.email "backskin@outlook.com"
-git config --global user.name "BackSkin-Pi"
+git config --global user.name "backskin-pi"
 while /bin/true; do
     echo "Update-Task: Pull...";
     git pull;
     echo "Trying to gather all data and commit";
     git add --all;
-    git commit -a -m "auto-commit (RP)::probably temp only";
+    git commit --author "backskin-pi"  -a -m "auto-commit (RP)::probably temp only";
     git push;
     echo "Update-Task: Objectives done";
     echo "Waiting for 2 minutes...";
