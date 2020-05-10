ef2de3c0b59114f14ced7f24b06003f67f64edf3
backskin <backskin@outlook.com>
2020-05-10 22:00:57 +0500

    auto-commit

--- stuff/update.bat
+++
@@ -1,8 +0,0 @@
-@echo Запущен автоматический скрипт
-@echo Обновление, Коммит и Пуш проекта
-@echo БАЛДЫНДА DiPy 
-@echo github.com/backskin/DiPy
-
-git pull
-git commit -a -m "auto-commit"
-git push
--- stuff/update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd ~/DiPy
-git pull
-git add --all
-git commit -a -m "auto-commit (RP)"
-git push
